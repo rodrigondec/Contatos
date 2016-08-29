5f396a10a37381acea0c2206662b1fcff89fdba5
Rodrigo Castro <rodrigondec@gmail.com>
2016-08-29 14:14:45 -0300

    adicionado servlet login

---
+++ src/main/java/servlet/LoginServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dominio.Usuario;
+
+/**
+ * Servlet implementation class LoginServlet
+ */
+@SuppressWarnings("serial")
+@WebServlet("/dologin")
+public class LoginServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) 
+			throws ServletException, IOException {
+			String login = request.getParameter("login");
+			String senha = request.getParameter("password");
+			
+			if(login.equals("admin") && senha.equals("admin")){
+				Usuario u = new Usuario(login, senha);
+				
+				
+				response.sendRedirect("/app/cadastro.html");
+			}
+			else{
+				response.sendRedirect("/app/login.html");
+			}
+			
+	}
+
+}
