25bb842fb3f247f1ff6b54998284c23dadd7f0b1
Rodrigo Castro <rodrigondec@gmail.com>
2016-08-29 14:33:08 -0300

    filtro adicionado

---
+++ src/main/java/filtro/LoginFilter.java
@@ -0,0 +1,47 @@
+package filtro;
+
+import java.io.IOException;
+
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dominio.Usuario;
+
+public class LoginFilter implements Filter {
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void doFilter(ServletRequest arg0, ServletResponse arg1, FilterChain arg2)
+			throws IOException, ServletException {
+		HttpServletRequest res = (HttpServletRequest) arg0;
+		HttpServletResponse resp = (HttpServletResponse) arg1;
+		
+		Usuario u = (Usuario) res.getSession().getAttribute("usuario");
+		
+		if(u == null){
+			resp.sendRedirect("app/login.html");
+		}
+		else{
+			arg2.doFilter(res, resp);
+		}
+
+	}
+
+	@Override
+	public void init(FilterConfig arg0) throws ServletException {
+		// TODO Auto-generated method stub
+
+	}
+
+}
